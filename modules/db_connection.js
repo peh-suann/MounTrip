9ed53c6d68562054c872f6353f364166f253a17d
ian <116136272+en-ian-0131@users.noreply.github.com>
2023-03-09 00:10:10 +0800

    03/09 00:10

--- modules/db_connection.js
+++ modules/db_connection.js
@@ -1,13 +1,17 @@
 const mysql = require('mysql2')
 const pool = mysql.createPool({
-  // host: 'localhost',
-  host: '192.168.21.84',
-  // user: 'root',
-  user: 'mountain',
-  // password: '',
-  password: 'mountaindude55',
+  host: 'localhost',
+  // host: '192.168.21.84',
+
+  user: 'root',
+  // user: 'mountain',
+
+  password: '',
+  // password: 'mountaindude55',
+
   // database: 'mountrip_dev',
   database: 'mountain',
+
   waitForConnections: true,
   connectionLimit: 5, //最多五人同時連線
   queueLimit: 0,
