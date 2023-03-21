323fc80f03f21353fa5bc53992289ee552e4fb36
Davispro <davispromail@gmail.com>
2023-03-21 14:16:40 +0800

    rating GROUP BY

--- modules/db_connection.js
+++ modules/db_connection.js
@@ -2,19 +2,20 @@ const mysql = require('mysql2')
 
 const pool = mysql.createPool({
   // yichun's connection
-  // host: '127.0.0.1',
-  // user: 'root',
-  // password: '',
-  // database: 'mountrip',
+  host: '127.0.0.1',
+  user: 'root',
+  password: '',
+  database: 'mountrip',
 
   // host: '192.168.21.84',
   // user: 'mountain',
   // password: 'mountaindude55',
+    // database: 'mountain',
 
-  host: '192.168.21.84',
-  user: 'mountain',
-  password: 'mountaindude55',
-  database: 'mountain',
+  // host: '192.168.21.84',
+  // user: 'mountain',
+  // password: 'mountaindude55',
+  // database: 'mountain',
   // host: 'localhost',
   // user: 'root',
   // password: '',
