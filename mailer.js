a02875f8503054fc06d1622828775fe633a5e074
kexin lu <lu773414@gmail.com>
2023-03-22 20:28:15 +0800

    彈跳視窗、mail發信測試

--- mailer.js
+++ mailer.js
@@ -1,16 +1,33 @@
-// const nodemailer = require('nodemailer')
+//引用 nodemailer
+var nodemailer = require('nodemailer');
 
+//宣告發信物件
+var transporter = nodemailer.createTransport({
+    service: 'Gmail',
+    auth: {
+        user: '',
+        pass: ''
+    }
+});
 
-// function sendMagicLinkEmail({email, token}) {
+var options = {
+    //寄件者
+    from: '',
+    //收件者
+    to: '', 
+    //主旨
+    subject: '這是 node.js 發送的測試信件', // Subject line
+    //純文字
+    text: 'Hello world2', // plaintext body
+    //嵌入 html 的內文
+    html: '<h2>HELLOW</h2> <p>The <a href="http://en.wikipedia.org/wiki/Lorem_ipsum" title="Lorem ipsum - Wikipedia, the free encyclopedia">Lorem ipsum</a> text is typically composed of pseudo-Latin words. It is commonly used as placeholder text to examine or demonstrate the visual effects of various graphic design. Since the text itself is meaningless, the viewers are therefore able to focus on the overall layout without being attracted to the text.</p>', 
+};
 
-//     nodemailer().sendMail({
-//         to: email,
-//         from: process.env.FROM_EMAIL,
-//         subject: "Reset Your Password",
-//         html: `<a href="http:/localhost:3000/verify?token=${token}">Click Here</a>`,
-//     })
-// }
-
-// module.exports = {
-//     sendMagicLinkEmail,
-// }
+//發送信件方法
+transporter.sendMail(options, function(error, info){
+    if(error){
+        console.log(error);
+    }else{
+        console.log('訊息發送: ' + info.response);
+    }
+});
